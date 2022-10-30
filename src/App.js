56d3f6524f5dc7cbeaac4ea2ecfb31734b3a3569
setykarimi <setykarimi80@gmail.com>
2022-10-30 12:32:43 +0330

    add categories and products state in app.js

--- src/App.js
+++ src/App.js
@@ -1,49 +1,23 @@
+import { useState } from 'react';
 import './App.css';
 import Category from './components/Category';
 import NavBar from './components/NavBar';
+import ProductList from './components/ProductList';
+import ProductsForm from './components/ProductsForm';
 
 function App() {
-  const products = [
-    {
-      id: 1,
-      title: "Node.js",
-      category: "backend",
-      createdAt: "2022-10-31T15:03:23.556Z"
-    },
-    {
-      id: 2,
-      title: "React.js",
-      category: "frontend",
-      createdAt: "2022-09-41T15:10:33.556Z"
-    },
-    {
-      id: 3,
-      title: "Vue.js",
-      category: "frontend",
-      createdAt: "2022-10-31T22:17:39.556Z"
-    }
-  ]
+  const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);
+  
 
-  const category = [
-    {
-      id: 1,
-      title: "frontend",
-      description: "frontend of applications",
-      createdAt: "2022-10-31T15:03:23.556Z"
-    },
-    {
-      id: 2,
-      title: "backend",
-      description: "the backend of the applications",
-      createdAt: "2021-12-56T56:09:43.556Z"
-    }
-  ]
 
   return (
     <div className='App bg-slate-500 min-h-screen'>
       <NavBar />
       <div className='container m-w-screen-sx m-auto '>
-        <Category />
+        <Category setCategories={setCategories}/>
+        <ProductsForm categories={categories} setProducts={setProducts}/>
+        <ProductList products={products} categories={categories} setProducts={setProducts}/>
       </div>
     </div>
   );
