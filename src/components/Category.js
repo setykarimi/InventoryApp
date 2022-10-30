312e4dac578c4d4919b2f945656d77b7e281b78c
setykarimi <setykarimi80@gmail.com>
2022-10-30 12:34:17 +0330

    remove categories state and add categories props instead

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,12 +1,12 @@
 import { useState } from "react";
 
-const Category = () => {
+const CategoryForm = ({setCategories}) => {
     const [isShow, setIsShow] = useState(false);
     const [categoryFormData, setCategoryFormData] = useState({
         title: "",
         description: ""
     });
-    const [categories, setCategories] = useState([]);
+   
 
     const changeInputHandler = ({target}) => {
         setCategoryFormData({...categoryFormData, [target.name] : target.value})
@@ -14,7 +14,7 @@ const Category = () => {
 
     const addNewCategoryHandler = (e) => {
         e.preventDefault();
-        const newCategory = {... categoryFormData, createdAt: new Date().toISOString()};
+        const newCategory = {... categoryFormData, createdAt: new Date().toISOString(), id: new Date().getTime()};
         // setCategories([...categories,  newCategory])
         setCategories(prevState => [...prevState,  newCategory]);
         setCategoryFormData({title: "", description: ""})
@@ -58,4 +58,4 @@ const Category = () => {
     );
 }
 
-export default Category;
+export default CategoryForm;
