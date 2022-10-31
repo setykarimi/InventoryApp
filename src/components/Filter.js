cebd161160c3584d0939dbb0968a21c3227d10d9
setykarimi <setykarimi80@gmail.com>
2022-10-31 10:33:16 +0330

    add selectCategory option

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,6 +1,13 @@
 import { useState } from "react";
 
-const Filter = ({ searchValue, sort, onSort, onSearch }) => {
+const Filter = ({
+    searchValue,
+    sort,
+    onSort,
+    onSearch,
+    categories,
+    selectedCategory,
+    onSelectCategory }) => {
 
     return (
         <div>
@@ -21,6 +28,21 @@ const Filter = ({ searchValue, sort, onSort, onSearch }) => {
                 >
                     <option value="latest">Latest</option>
                     <option value="earliest">Earliest</option>
+                </select>
+            </div>
+
+            <div>
+                <label>sort categori</label>
+                <select
+                    value={selectedCategory}
+                    onChange={onSelectCategory}
+                >
+                <option selected value="">All</option>
+
+                    {categories.map((category) => {
+                        return (<option key={category.id} value={category.id}>{category.title}</option>)
+                    })}
+
                 </select>
             </div>
         </div>
