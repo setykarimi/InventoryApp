6acd7420ba7afa5e3a91c43633324aee18977d40
setykarimi <setykarimi@gmail.com>
2022-11-01 22:34:15 +0330

    edit classNames for styling

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -10,42 +10,47 @@ const Filter = ({
     onSelectCategory }) => {
 
     return (
-        <div>
-            <div>
-                <label>Search</label>
-                <input
-                    type="search"
-                    value={searchValue}
-                    onChange={onSearch}
-                />
+        <section className="filter-section mb-6 bg-indigo-700 p-5 rounded-md">
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 justify-center items-center">
+                <div className="flex justify-between items-center space-x-2">
+                    <label className="text-md text-gray-100">Search</label>
+                    <input
+                        type="search"
+                        value={searchValue}
+                        onChange={onSearch}
+                        className="rounded-md py-1 border-0 focus:border-0 focus:outline-0 w-11/12"
+                    />
+                </div>
+
+                <div className="flex justify-between items-center space-x-2">
+                    <label className="text-md text-gray-100">Sort: </label>
+                    <select
+                        value={sort}
+                        onChange={onSort}
+                        className="rounded-md py-1 border-0 focus:border-0 focus:outline-0 w-11/12"
+                    >
+                        <option value="latest">Latest</option>
+                        <option value="earliest">Earliest</option>
+                    </select>
+                </div>
+
+                <div className="flex justify-between items-center space-x-2">
+                    <label className="text-md text-gray-100">Categori: </label>
+                    <select
+                        value={selectedCategory}
+                        onChange={onSelectCategory}
+                        className="rounded-md py-1 border-0 focus:border-0 focus:outline-0 w-11/12"
+                    >
+                        <option selected value="">All</option>
+
+                        {categories.map((category) => {
+                            return (<option key={category.id} value={category.id}>{category.title}</option>)
+                        })}
+
+                    </select>
+                </div>
             </div>
-
-            <div>
-                <label>sort</label>
-                <select
-                    value={sort}
-                    onChange={onSort}
-                >
-                    <option value="latest">Latest</option>
-                    <option value="earliest">Earliest</option>
-                </select>
-            </div>
-
-            <div>
-                <label>sort categori</label>
-                <select
-                    value={selectedCategory}
-                    onChange={onSelectCategory}
-                >
-                <option selected value="">All</option>
-
-                    {categories.map((category) => {
-                        return (<option key={category.id} value={category.id}>{category.title}</option>)
-                    })}
-
-                </select>
-            </div>
-        </div>
+        </section>
     );
 }
 
