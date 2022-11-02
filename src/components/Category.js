5b46405233ab7dc5302ca57d6252e3970b105a21
setykarimi <setykarimi80@gmail.com>
2022-11-02 11:49:06 +0330

    edit grid classes for responsive view

--- src/components/Category.js
+++ src/components/Category.js
@@ -24,7 +24,7 @@ const CategoryForm = ({ setCategories }) => {
             <div className={`mb-6 bg-indigo-500 p-5 rounded-md ${isShow ? "" : "hidden"}`}>
                 <h2 className="font-bold text-lg text-left text-gray-100 mb-3">Add New Category</h2>
 
-                <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 justify-center items-center">
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-5 justify-center items-center">
                     <div className="flex justify-between items-center space-x-2">
                         <label className="text-md text-gray-100">Title: </label>
                         <input
@@ -32,10 +32,10 @@ const CategoryForm = ({ setCategories }) => {
                             name="title"
                             value={categoryFormData.title}
                             onChange={changeInputHandler}
-                            className="rounded-md py-1 border-0 focus:border-0 focus:outline-0 w-60"
+                            className="rounded-md py-1 border-0 focus:border-0 focus:outline-0 w-full"
                         />
                     </div>
-                    <div className="flex lg:col-span-2 justify-between items-center space-x-2">
+                    <div className="flex md:col-span-2 justify-between items-center space-x-2">
                         <label className="text-md text-gray-100">Description: </label>
                         <textarea
                             name="description"
@@ -45,12 +45,12 @@ const CategoryForm = ({ setCategories }) => {
                         />
                     </div>
                     <button
-                        className="border border-indigo-200 py-2 font-bold rounded-md text-indigo-200 hover:bg-indigo-200 hover:text-indigo-600 transition ease-in lg:col-start-1 lg:col-end-2"
+                        className="border border-indigo-200 py-2 font-bold rounded-md text-indigo-200 hover:bg-indigo-200 hover:text-indigo-600 transition ease-in md:col-start-1 md:col-end-2"
                         onClick={() => setIsShow(false)}>
                         Cancel
                     </button>
                     <button
-                        className="bg-indigo-200 py-2 font-bold rounded-md border border-indigo-200 text-indigo-600 lg:col-start-3 lg:col-end-4"
+                        className="bg-indigo-200 py-2 font-bold rounded-md border border-indigo-200 text-indigo-600 md:col-start-3 md:col-end-4"
                         onClick={addNewCategoryHandler}>
                         Add Category
                     </button>
