4772ba3d2d72016e550f717a457d0c1cd1ff5b52
setykarimi <setykarimi80@gmail.com>
2022-11-02 11:50:04 +0330

    edit classNames in productList for responsive view

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -13,14 +13,14 @@ const ProductList = ({ products, categories, setProducts }) => {
             {
                 products.map(product => {
                     return (
-                        <div key={product.id} className="lg:grid lg:grid-cols-2 flex text-left space-y-3">
-                            <span className="font-bold text-lg">{product.title}</span>
-                            <div className="flex justify-between">
+                        <div key={product.id} className= "flex justify-between text-left space-y-3 items-center">
+                            <span className="font-bold text-lg mt-2">{product.title}</span>
+                            <div className="flex space-x-3 items-center mt-0">
                                 <span>{new Date(product.createdAt).toLocaleDateString("fa-IR")}</span>
                                 <span>{findCategory(product.categoryId)}</span>
                                 <span className="flex justify-center items-center font-bold text-indigo-500 rounded-full bg-indigo-200 w-6 h-6">{product.quantity}</span>
                                 <button 
-                                className="border border-red-500 text-red-500 px-3 rounded"
+                                className="border border-red-500 text-red-500 px-2 py-1 rounded font-bold text-sm"
                                 onClick={() => deleteproductHandler(product.id)}>delete</button>
                             </div>
                         </div>
