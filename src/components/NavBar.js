bd6a79e9bd16ca5e2777a21262ca4c7fcf3be2ee
setykarimi <setykarimi80@gmail.com>
2022-11-02 11:55:33 +0330

    add productsLength to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,9 @@
-const NavBar = () => {
+const NavBar = ({products}) => {
+    
     return ( 
-        <nav className="text-center bg-indigo-500 py-3">
+        <nav className="text-center bg-indigo-500 py-3 flex justify-center items-center">
             <h1 className="text-2xl font-bold text-white">Inventory App</h1>
+            <span className="ml-3 w-6 h-6 bg-indigo-100 rounded-full text-indigo-500 font-bold">{products.length}</span>
         </nav>
      );
 }
