3d5f82c7f1e055067812418e89f9c041ac475d08
setykarimi <setykarimi80@gmail.com>
2022-11-28 14:56:51 +0330

    Add yup for formValidation, and set error for inputs, disabled for submit button

--- src/components/ProductsForm.js
+++ src/components/ProductsForm.js
@@ -1,7 +1,8 @@
 import { useFormik } from "formik";
+import * as Yup from 'yup';
 
 const ProductsForm = ({ categories, setProducts }) => {
-   
+
     const handleSubmit = (values) => {
         console.log(values);
 
@@ -25,57 +26,78 @@ const ProductsForm = ({ categories, setProducts }) => {
         categoryId: ""
     }
 
+    const validationSchema = Yup.object({
+        title: Yup.string().required("Title is required"),
+        quantity: Yup.number().required("Quantity is required"),
+        categoryId: Yup.string().required("Category is required")
+    })
+
     const formik = useFormik({
         initialValues: initialValues,
         onSubmit: handleSubmit,
-        enableReinitialize: true
+        validationSchema: validationSchema,
+        enableReinitialize: true,
+        validateOnMount: true
+
     })
 
     return (
         <section className="product-section">
-           <form onSubmit={formik.handleSubmit}>
+            <form onSubmit={formik.handleSubmit}>
                 <div className="mb-6 bg-indigo-600 p-5 rounded-md">
                     <h2 className="font-bold text-lg text-left text-gray-100 mb-3">Add New Product</h2>
 
                     <div className="grid grid-cols-1 md:grid-cols-3 gap-5 justify-center items-center">
-                        <div className="flex justify-between items-center space-x-2">
-                            <label className="text-md text-gray-100">Title: </label>
-                            <input
-                                type="text"
-                                name="title"
-                                className="rounded-md py-1 border-0 focus:border-0 focus:outline-0 w-11/12"
-                                {...formik.getFieldProps('title')}
-                            />
+                        <div className="flex flex-col relative">
+                            <div className="flex justify-between items-center space-x-2">
+                                <label className="text-md text-gray-100">Title: </label>
+                                <input
+                                    type="text"
+                                    name="title"
+                                    className="rounded-md py-1 border-0 focus:border-0 focus:outline-0 w-11/12"
+                                    {...formik.getFieldProps('title')}
+                                />
+                            </div>
+                            {formik.errors.title && formik.touched.title &&
+                                <p className="absolute top-8 text-red-500 text-sm">{formik.errors.title}</p>}
                         </div>
 
-                        <div className="flex justify-between items-center space-x-2">
-                            <label className="text-md text-gray-100">Quantity: </label>
-                            <input
-                                type="number"
-                                name="quantity"
-                                className="rounded-md py-1 border-0 focus:border-0 focus:outline-0 w-11/12"
-                                {...formik.getFieldProps('quantity')}
-                            />
+                        <div className="flex flex-col relative">
+                            <div className="flex justify-between items-center space-x-2">
+                                <label className="text-md text-gray-100">Quantity: </label>
+                                <input
+                                    type="number"
+                                    name="quantity"
+                                    className="rounded-md py-1 border-0 focus:border-0 focus:outline-0 w-11/12"
+                                    {...formik.getFieldProps('quantity')}
+                                />
+                            </div>
+                            {formik.errors.quantity && formik.touched.quantity &&
+                                <p className="absolute top-8 text-red-500 text-sm">{formik.errors.quantity}</p>}
                         </div>
 
-                        <div className="flex justify-between items-center space-x-2">
-                            <label className="text-md text-gray-100">Category: </label>
-                            <select
-                                name="categoryId"
-                                className="rounded-md py-1 border-0 focus:border-0 focus:outline-0 w-11/12"
-                                {...formik.getFieldProps('categoryId')}
-                            >
-                                <option>select a category</option>
-                                {categories && categories.map((category) => {
-                                    return (
-                                        <option name="categoryId" key={category.id} value={category.id}>{category.title}</option>
-                                    )
-                                })}
-                            </select>
+                        <div className="flex flex-col relative">
+                            <div className="flex justify-between items-center space-x-2">
+                                <label className="text-md text-gray-100">Category: </label>
+                                <select
+                                    name="categoryId"
+                                    className="rounded-md py-1 border-0 focus:border-0 focus:outline-0 w-11/12"
+                                    {...formik.getFieldProps('categoryId')}
+                                >
+                                    <option selected>select a category</option>
+                                    {categories && categories.map((category) => {
+                                        return (
+                                            <option name="categoryId" key={category.id} value={category.id}>{category.title}</option>
+                                        )
+                                    })}
+                                </select>
+                            </div>
+                            {formik.errors.categoryId && formik.touched.categoryId &&
+                                <p className="absolute top-8 text-red-500 text-sm">{formik.errors.categoryId}</p>}
                         </div>
 
                         <button
-                            className="bg-indigo-200 py-2 font-bold rounded-md border border-indigo-200 text-indigo-600 sx:col-start-2 sx:col-end-3"
+                            className="bg-green-200 py-2 mt-2 font-bold rounded-md border border-green-200 text-green-600 sx:col-start-2 sx:col-end-3 disabled:text-gray-500 disabled:bg-gray-300 disabled:border-0"
                             type="submit"
                             disabled={!formik.isValid}>Add new Product
                         </button>
